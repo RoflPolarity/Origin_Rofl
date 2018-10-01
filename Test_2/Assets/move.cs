39c5f0d07bb0e82aceafc5e57dc77bc5db658f77
Reys <ignis604@gmail.com>
2018-10-01 21:21:07 +0300

    f

--- Test_2/Assets/move.cs
+++ Test_2/Assets/move.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class move : MonoBehaviour {
+﻿using UnityEngine;
+using System.Collections;
+public class Move : MonoBehaviour
+{
     public GameObject player;
-    public int speed = 2;
     public int speedRotation = 3;
-	void Start () {
+    public int speed = 5;
+    public AnimationClip anima;
+    public int jumpSpeed = 50;
+
+    void Start()
+    {
         player = (GameObject)this.gameObject;
-	}
-	void Update () {
-		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            player.transform.position += player.transform.up * speed * Time.deltaTime;
+            player.transform.position += player.transform.forward * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
@@ -25,6 +29,10 @@ public class move : MonoBehaviour {
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             player.transform.Rotate(Vector3.up * speedRotation);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            player.transform.position += player.transform.up * jumpSpeed * Time.deltaTime;
         }
     }
 }
