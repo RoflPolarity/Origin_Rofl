e685f56b92e345df3d7d9868d52c536425dbb02f
Temaxachkeks <killerok2285@mail.ru>
2018-10-05 15:39:12 +0400

    lox3

---
+++ Test_2/Assets/E.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E : MonoBehaviour {
+    public GameObject Obj;
+    public string NameFunk;
+    void Start () {
+    }
+	void OnTriggerEnter (Collider mane_char)
+    {
+       // Obj = GetComponent<MeshRenderer>().enabled;
+        if (mane_char.tag == "Player")
+        {
+            Debug.Log("Privet");
+            // if (Input.GetKeyDown(KeyCode.E))
+            //{
+            //if (Obj == true)
+            //{
+              //  Obj = false;
+            }
+           
+        }
+    void OnTriggerStay (Collider mane_char)
+    {
+        if (mane_char.tag == "Player")
+        {
+            Debug.Log("Privet");
+        }
+    }
+        //}
+    }
