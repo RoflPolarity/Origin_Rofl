38911096fa908529af320903547c3065fc677341
Reys <ignis604@gmail.com>
2018-10-06 16:53:15 +0300

    anim

--- Test_2/Assets/move.cs
+++ Test_2/Assets/move.cs
@@ -2,55 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move : MonoBehaviour {
-    public GameObject Obj;
-    private Animation Left,Rihgt,on_user,ot_user;
-    public string nameanim_1, nameanim_2, nameanim_3, nameanim_4;
+public class move : MonoBehaviour
+{
+    private Animator animat;
     public float horizantalSpeed, verticalSpeed;
     float speedX, speedY;
-    void Start() {
-
+    void Start()
+    {
+        animat = GetComponent<Animator>();
     }
 
-    void Update() {
+    void Update()
+    {
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             speedX = -horizantalSpeed;
+            animat.Play("Left");
+            animat.Play("stay");
         }
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             speedX = horizantalSpeed;
+            animat.Play("Right");
+            animat.("stay");
         }
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             speedY = verticalSpeed;
+            animat.Play("ot_user");
+            animat.Play("stay");
         }
         else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        { 
+        {
             speedY = -verticalSpeed;
+            animat.Play("on_user");
+            animat.Play("state");
         }
         transform.Translate(speedX, speedY, 0);
         speedX = 0;
         speedY = 0;
-        anim();
-}
-    void anim() {
-       // Left = Obj.GetComponent<Animation>().Play ("");
-    
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            Obj.GetComponent<Animation>().Play("Left");
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-        }
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-        }
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            Obj.GetComponent<Animation>().Play(nameanim_2);
-        }
-
     }
 }
+    
