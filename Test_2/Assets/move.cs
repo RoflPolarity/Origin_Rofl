1e97f837d68975976367baeabbacb14380705d1b
Temaxachkeks <killerok2285@mail.ru>
2018-10-08 22:33:29 +0400

    tema1

--- Test_2/Assets/move.cs
+++ Test_2/Assets/move.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
+    private PolygonCollider2D coll, paths, element0;
+    Vector2 points;
+    private GameObject ManeChar;
     private Animator animat;
     public float horizantalSpeed, verticalSpeed;
     float speedX, speedY;
+    private string lastNav = "onUser";
     void Start()
+
     {
+        ManeChar = GameObject.Find("char");
+        coll = ManeChar.GetComponent<PolygonCollider2D>();
         animat = GetComponent<Animator>();
     }
 
@@ -17,30 +24,62 @@ public class move : MonoBehaviour
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             speedX = -horizantalSpeed;
-            animat.Play("Left");
-            animat.Play("stay");
         }
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             speedX = horizantalSpeed;
-            animat.Play("Right");
-            animat.("stay");
         }
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             speedY = verticalSpeed;
-            animat.Play("ot_user");
-            animat.Play("stay");
         }
         else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             speedY = -verticalSpeed;
-            animat.Play("on_user");
-            animat.Play("state");
         }
         transform.Translate(speedX, speedY, 0);
         speedX = 0;
         speedY = 0;
+        anim();
+    }
+    void anim()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            animat.Play("Right");
+            lastNav = "Right";
+        }
+        else if (lastNav.Equals("Right"))
+        {
+            animat.Play("Right_stay");
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            animat.Play("Left");
+            lastNav = "Left";
+        }
+        else if (lastNav.Equals("Left"))
+        {
+            animat.Play("Left_stay");
+        }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            animat.Play("otUser");
+            lastNav = "otUser";
+        }
+        else if (lastNav.Equals("otUser"))
+        {
+            animat.Play("otUser_stay");
+
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            animat.Play("onUser");
+            lastNav = "onUser";
+        }
+        else if (lastNav.Equals("onUser"))
+        {
+            animat.Play("onUser_stay");
+        }
     }
 }
-    
