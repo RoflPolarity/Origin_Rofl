2d5b1f7174cb8e2ff881abed3c434671407695ae
RoflPolarity <43727574+RoflPolarity@users.noreply.github.com>
2019-01-13 21:14:30 +0300

    Казахи сосут

--- src/leonid.java
+++ src/leonid.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class leonid  {
     public static void main(String[] args){
@@ -13,15 +15,19 @@ public class leonid  {
             String [][] array = {{"Янпольский Артём Олегович", "Физмат"},{"Чеков Леонид Анатольевич","Физмат"}};
         JButton deleteButton = new JButton("Назад");
         JButton clearButton = new JButton("Сформировать отчет");
-
         BookTableModel btm = new BookTableModel();
         JTable bookTable = new JTable(array,names);
         JScrollPane bookTableScrollPane = new JScrollPane(bookTable);
         bookTableScrollPane.setPreferredSize(new Dimension(400,400));
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(array[0][1]);
+            }
+        });
         frame.add(bookTableScrollPane);
         frame.add(deleteButton);
         frame.add(clearButton);
-
         frame.setVisible(true);
         frame.pack();
         frame.revalidate();
