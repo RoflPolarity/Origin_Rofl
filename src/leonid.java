2be6d64fb49154ee084526563e25fb32ae6ce43d
Leonidiuz <leonid.cherkov2000@gmail.com>
2019-01-13 18:30:05 +0300

    папаыв
    пкыпаыв

--- src/leonid.java
+++ src/leonid.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class leonid {
+public class leonid  {
     public static void main(String[] args){
         JFrame frame = new JFrame ("Legion");
         frame.setSize(new Dimension(600, 400));
