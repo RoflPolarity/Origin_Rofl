75bec6fb00d48f1c57f26777171e9d5fbbfda727
RoflPolarity <43727574+RoflPolarity@users.noreply.github.com>
2019-01-13 21:00:25 +0300

    Казахи сосут

--- src/leonid.java
+++ src/leonid.java
@@ -3,28 +3,22 @@ import java.awt.*;
 
 public class leonid  {
     public static void main(String[] args){
+
         JFrame frame = new JFrame ("Legion");
         frame.setSize(new Dimension(600, 400));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setLayout(new GridBagLayout());
-
+            String names [] = {"ФИО","Профиль"};
+            String [][] array = {{"Янпольский Артём Олегович", "Физмат"},{"Чеков Леонид Анатольевич","Физмат"}};
         JButton deleteButton = new JButton("Назад");
         JButton clearButton = new JButton("Сформировать отчет");
 
         BookTableModel btm = new BookTableModel();
-        JTable bookTable = new JTable(btm);
+        JTable bookTable = new JTable(array,names);
         JScrollPane bookTableScrollPane = new JScrollPane(bookTable);
         bookTableScrollPane.setPreferredSize(new Dimension(400,400));
-            Main prog = new Main();
-            prog.open();
-            prog.select_people();
-                //System.out.println(prog.select_people());
-       // btm.addDate();
-
-        frame.add(bookTableScrollPane, new GridBagConstraints(0,0,3,1,1,1,
-                GridBagConstraints.NORTH, GridBagConstraints.BOTH,
-                new Insets(1,1,1,1),0,0));
+        frame.add(bookTableScrollPane);
 
         frame.add(deleteButton, new GridBagConstraints(1,1,1,1,1,1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
