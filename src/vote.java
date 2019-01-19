eb432f2fdf3bf8c7566fef3da23daefe538f3135
RoflPolarity <43727574+RoflPolarity@users.noreply.github.com>
2019-01-19 13:44:43 +0300

    Table

--- src/vote.java
+++ src/vote.java
@@ -6,7 +6,7 @@ public class vote {
     Main vote=new Main();
     Connection co;
     GUI_1 gui = new GUI_1();
-    public String vale (String name){
+    public  void vale (String name){
         String  out;
         try {
             vote.open();
@@ -15,10 +15,8 @@ public class vote {
         ResultSet rs = st.executeQuery(query);
         String classes = rs.getString("Классы");
         String [] words = classes.split(",");
-        out = words[0];
-        return out;
-        }
 
+        }
         catch (Exception e){
             System.out.println(e.getMessage());
         }
